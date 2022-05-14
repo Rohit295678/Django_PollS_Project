d4058678061e4891a774125ef8d853b36b12eb75
Rohit295678 <104978818+Rohit295678@users.noreply.github.com>
2022-05-14 13:00:58 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('polls.urls'))
+    path('', views.index),
+    path('<int:questions_id>',views.showchoices, name='showchoices'),
+    path('<int:questions_id>/result', views.voted, name='voted'),
 ]
